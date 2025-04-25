ce0221da81709d6d4c1b4599c7b112f2293f3b49
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-25 19:02:54 -0300

    42: adjustng validate enrollment in UpdateUserProfileInformation

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -22,7 +22,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png,webp', 'max:2048'],
             
-            'enrollment' => ['required', 'string', 'max:9', 'min:9', 'regex:/^VP\d{7}$/i'],
+            'enrollment' => ['required', 'string', 'max:9', 'min:9', 'regex:/^VP\d{7}$/i', Rule::unique('users')->ignore($user->id)],
             'course' => ['required', Rule::in([
                 'Informática',
                 'Mecatrônica',
